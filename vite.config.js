3eaa9465c3d4f3be7933dddc8ad5840d21c7cd74
aleql <aquijada@dcc.uchile.cl>
2022-09-09 16:18:25 -0400

    updated domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aleql.github.io/',
+  base: '/eye_search_home/',
   plugins: [react()]
 })
